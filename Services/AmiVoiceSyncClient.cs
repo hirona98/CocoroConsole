a8a267fded3809f2f7c35852986b43d57dc2f894
hirona <hiro.98v7@gmail.com>
2026-02-12 22:06:43 +0900

    バージョン変更

--- Services/AmiVoiceSyncClient.cs
+++ Services/AmiVoiceSyncClient.cs
@@ -35,7 +35,7 @@ namespace CocoroConsole.Services
 
             // Keep-Alive設定
             _httpClient.DefaultRequestHeaders.Connection.Add("keep-alive");
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/5.0.0");
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/5.0.1");
         }
 
         /// <summary>
