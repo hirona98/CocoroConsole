f78a08d2118e4ba41399ab98cf08f319b52ef43f
hirona <hiro.98v7@gmail.com>
2026-05-23 18:16:33 +0900

    デスクトップウォッチが抜けていたため修正

--- Controls/SystemSettingsControl.xaml.cs
+++ Controls/SystemSettingsControl.xaml.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,6 +15,8 @@ namespace CocoroConsole.Controls
     public partial class SystemSettingsControl : UserControl
     {
         public event EventHandler? SettingsChanged;
+
+        private const string DesktopWakeObservationId = "observation:main_desktop";
 
         private bool _isInitialized;
         private Dictionary<string, object?> _wakePolicy = new Dictionary<string, object?>();
@@ -66,6 +70,7 @@ namespace CocoroConsole.Controls
             var mode = ReadString(wakePolicy, "mode");
             WakePolicyEnabledCheckBox.IsChecked = string.Equals(mode, "interval", StringComparison.OrdinalIgnoreCase);
             WakeIntervalSecondsTextBox.Text = ReadInt(wakePolicy, "interval_seconds", 300).ToString(CultureInfo.InvariantCulture);
+            WakeDesktopObservationCheckBox.IsChecked = HasDesktopWakeObservation(wakePolicy);
         }
 
         private void ApplyDefaultRemoteSettings()
@@ -75,6 +80,7 @@ namespace CocoroConsole.Controls
                 ["mode"] = "disabled",
             };
             WakePolicyEnabledCheckBox.IsChecked = false;
+            WakeDesktopObservationCheckBox.IsChecked = false;
             WakeIntervalSecondsTextBox.Text = "300";
         }
 
@@ -84,6 +90,8 @@ namespace CocoroConsole.Controls
             ExcludeWindowTitlePatternsTextBox.TextChanged += OnSettingsChanged;
             WakePolicyEnabledCheckBox.Checked += OnSettingsChanged;
             WakePolicyEnabledCheckBox.Unchecked += OnSettingsChanged;
+            WakeDesktopObservationCheckBox.Checked += OnSettingsChanged;
+            WakeDesktopObservationCheckBox.Unchecked += OnSettingsChanged;
             WakeIntervalSecondsTextBox.TextChanged += OnSettingsChanged;
             MicThresholdSlider.ValueChanged += OnSettingsChanged;
         }
@@ -111,11 +119,169 @@ namespace CocoroConsole.Controls
 
                 wakePolicy["mode"] = "interval";
                 wakePolicy["interval_seconds"] = intervalSeconds;
+                ApplyDesktopWakeObservation(wakePolicy, WakeDesktopObservationCheckBox.IsChecked ?? false);
                 return wakePolicy;
             }
 
             wakePolicy["mode"] = "disabled";
+            ApplyDesktopWakeObservation(wakePolicy, WakeDesktopObservationCheckBox.IsChecked ?? false);
             return wakePolicy;
+        }
+
+        private static bool HasDesktopWakeObservation(Dictionary<string, object?> wakePolicy)
+        {
+            return ReadWakeObservations(wakePolicy.GetValueOrDefault("observations"))
+                .Any(IsDesktopWakeObservation);
+        }
+
+        private static void ApplyDesktopWakeObservation(Dictionary<string, object?> wakePolicy, bool enabled)
+        {
+            var observations = ReadWakeObservations(wakePolicy.GetValueOrDefault("observations"))
+                .Where(observation => !IsDesktopWakeObservation(observation))
+                .Cast<object?>()
+                .ToList();
+
+            if (enabled)
+            {
+                observations.Add(BuildDesktopWakeObservation());
+            }
+
+            if (observations.Count > 0)
+            {
+                wakePolicy["observations"] = observations;
+            }
+            else
+            {
+                wakePolicy.Remove("observations");
+            }
+        }
+
+        private static Dictionary<string, object?> BuildDesktopWakeObservation()
+        {
+            return new Dictionary<string, object?>
+            {
+                ["observation_id"] = DesktopWakeObservationId,
+                ["enabled"] = true,
+                ["capability_id"] = "vision.capture",
+                ["input"] = new Dictionary<string, object?>
+                {
+                    ["vision_source_id"] = BuildDesktopVisionSourceId(),
+                    ["mode"] = "still",
+                },
+            };
+        }
+
+        private static string BuildDesktopVisionSourceId()
+        {
+            return $"vision_source:{NormalizeVisionSourceToken(AppSettings.Instance.ClientId)}:desktop";
+        }
+
+        private static string NormalizeVisionSourceToken(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return "console";
+            }
+
+            var builder = new StringBuilder(value.Length);
+            foreach (var ch in value.Trim())
+            {
+                if (char.IsLetterOrDigit(ch) || ch == '-' || ch == '_')
+                {
+                    builder.Append(ch);
+                }
+            }
+
+            return builder.Length == 0 ? "console" : builder.ToString();
+        }
+
+        private static bool IsDesktopWakeObservation(Dictionary<string, object?> observation)
+        {
+            var observationId = ReadString(observation, "observation_id");
+            if (string.Equals(observationId, DesktopWakeObservationId, StringComparison.Ordinal))
+            {
+                return true;
+            }
+
+            if (!string.Equals(ReadString(observation, "capability_id"), "vision.capture", StringComparison.Ordinal))
+            {
+                return false;
+            }
+
+            var input = ReadObject(observation.GetValueOrDefault("input"));
+            var sourceId = ReadString(input, "vision_source_id");
+            return string.Equals(sourceId, BuildDesktopVisionSourceId(), StringComparison.Ordinal);
+        }
+
+        private static List<Dictionary<string, object?>> ReadWakeObservations(object? value)
+        {
+            if (value is JsonElement element)
+            {
+                return ReadWakeObservations(element);
+            }
+
+            if (value is IEnumerable<object?> objects)
+            {
+                return objects
+                    .Select(ReadObject)
+                    .Where(item => item.Count > 0)
+                    .ToList();
+            }
+
+            return new List<Dictionary<string, object?>>();
+        }
+
+        private static List<Dictionary<string, object?>> ReadWakeObservations(JsonElement element)
+        {
+            if (element.ValueKind != JsonValueKind.Array)
+            {
+                return new List<Dictionary<string, object?>>();
+            }
+
+            return element.EnumerateArray()
+                .Select(ReadObject)
+                .Where(item => item.Count > 0)
+                .ToList();
+        }
+
+        private static Dictionary<string, object?> ReadObject(object? value)
+        {
+            if (value is Dictionary<string, object?> dictionary)
+            {
+                return new Dictionary<string, object?>(dictionary);
+            }
+
+            if (value is JsonElement element)
+            {
+                return ReadObject(element);
+            }
+
+            return new Dictionary<string, object?>();
+        }
+
+        private static Dictionary<string, object?> ReadObject(JsonElement element)
+        {
+            if (element.ValueKind != JsonValueKind.Object)
+            {
+                return new Dictionary<string, object?>();
+            }
+
+            return element.EnumerateObject()
+                .ToDictionary(property => property.Name, property => ReadJsonValue(property.Value));
+        }
+
+        private static object? ReadJsonValue(JsonElement element)
+        {
+            return element.ValueKind switch
+            {
+                JsonValueKind.Object => ReadObject(element),
+                JsonValueKind.Array => element.EnumerateArray().Select(ReadJsonValue).ToList(),
+                JsonValueKind.String => element.GetString(),
+                JsonValueKind.Number => element.TryGetInt32(out var intValue) ? intValue : element.GetDouble(),
+                JsonValueKind.True => true,
+                JsonValueKind.False => false,
+                _ => null,
+            };
         }
 
         public int GetVisualCaptureIdleTimeoutMinutes()
